658916190c6f707e8ae079e2c5b2d1407579a295
unknown <rmtestard@gmail.com>
2019-07-02 19:59:02 +0200

    code splitting with System.import()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import sum from './sum';
-import './image_viewer';
+// import sum from './sum';
+// import './image_viewer';
 
-console.log(sum(4, 5));
+// console.log(sum(4, 5));
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+  System.import('./image_viewer').then(module => {
+    module.default();
+  });
+};
+
+document.body.appendChild(button);
