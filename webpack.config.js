c1f499a6d0a2db5bf37af9acfefc68b36b2de0e6
unknown <rmtestard@gmail.com>
2019-07-02 06:58:57 +0200

    implement babel to load ES2015

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        exclude: /node_modules/,
+        test: /\.js$/
+      }
+    ]
   }
 };
